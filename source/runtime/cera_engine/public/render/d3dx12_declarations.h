21757c7e6467942264349cac5ce72ac89164649d
Dyronix <davedebreuck.gamedeveloper@gmail.com>
2024-05-06 14:32:20 +0200

    CERA - Engine API

--- source/runtime/cera_engine/public/render/d3dx12_declarations.h
+++
@@ -1,9 +0,0 @@
-// DirectX 12 specific headers.
-#include <d3d12.h>
-#include <dxgi1_6.h>
-#include <d3dcompiler.h>
-#include <DirectXMath.h>
-#include <DirectXTex.h>
-
-// D3D12 extension library.
-#include "d3dx12.h"
